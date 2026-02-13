a29da76f8ea065937b7e74ea5576383a0b6e24af
rgm <git@rushd.xyz>
2026-02-13 18:31:26 +0000

    Java exceptions and translation to HTTP responses

--- src/main/java/xyz/rushd/spring_calculator/CalculatorController.java
+++ src/main/java/xyz/rushd/spring_calculator/CalculatorController.java
@@ -1,9 +1,13 @@
 package xyz.rushd.spring_calculator;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping
@@ -17,7 +21,14 @@ public class CalculatorController {
       case "+" -> add(firstNumber, secondNumber);
       case "-" -> sub(firstNumber, secondNumber);
       case "*" -> mul(firstNumber, secondNumber);
-      case "/" -> div(firstNumber, secondNumber);
+      case "/" -> {
+        if (secondNumber == 0) {
+          throw new IllegalArgumentException("Division by zero: To infinity and beyond, "
+              + "as the second number = 0! " + "🚀");
+        } else {
+          yield div(firstNumber, secondNumber);
+        }
+      }
       default -> {
         throw new IllegalArgumentException("Invalid operation selected, please type \"+\" to add, "
             + "\"-\" to subtract, \"*\" to multiply or \"/\" to divide next time.");
@@ -45,5 +56,12 @@ public class CalculatorController {
   @GetMapping("/div")
   public int div(@RequestParam int firstNumber, @RequestParam int secondNumber) {
     return firstNumber / secondNumber;
+  }
+
+  // Translate Java IllegalArgumentException into HTTP responses
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ExceptionHandler(IllegalArgumentException.class)
+  public String handleBadRequest(IllegalArgumentException ex) {
+    return ex.getMessage();
   }
 }
