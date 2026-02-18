12e34f5592e51c9c9a5c260baa3cf8b37dbff396
rgm <git@rushd.xyz>
2026-02-18 11:17:25 +0000

    Created controller and service packages

--- src/main/java/xyz/rushd/service_layer/controller/CalculatorController.java
+++ src/main/java/xyz/rushd/service_layer/controller/CalculatorController.java
@@ -1,9 +1,10 @@
-package xyz.rushd.service_layer;
+package xyz.rushd.service_layer.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import xyz.rushd.service_layer.Calculator;
 
 
 @RestController
---
+++ src/main/java/xyz/rushd/service_layer/service/CalculatorService.java
@@ -0,0 +1,8 @@
+package xyz.rushd.service_layer.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+
+}
