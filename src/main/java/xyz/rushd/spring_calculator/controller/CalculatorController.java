19389826f8db866733a818c600a2c76425abdb69
rgm <git@rushd.xyz>
2026-02-23 14:18:39 +0000

    Response Entities

--- src/main/java/xyz/rushd/spring_calculator/controller/CalculatorController.java
+++ src/main/java/xyz/rushd/spring_calculator/controller/CalculatorController.java
@@ -1,5 +1,6 @@
 package xyz.rushd.spring_calculator.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,33 +19,41 @@ public class CalculatorController {
   }
 
   @GetMapping("/calculate")
-  public Calculator calculate(@RequestParam int firstNumber, @RequestParam String operation,
-      @RequestParam int secondNumber) {
+  public ResponseEntity<Calculator> calculate(@RequestParam int firstNumber,
+      @RequestParam String operation, @RequestParam int secondNumber) {
     Calculator calculator = new Calculator(firstNumber, operation, secondNumber);
     calculator.setResult(calculatorService.operation(firstNumber, operation, secondNumber));
-    return calculator;
+    return ResponseEntity.ok(calculator);
   }
 
   // Redundant endpoints for testing
 
   @GetMapping("/add")
-  public int add(@RequestParam int firstNumber, @RequestParam int secondNumber) {
-    return firstNumber + secondNumber;
+  public ResponseEntity<Integer> add(@RequestParam int firstNumber,
+      @RequestParam int secondNumber) {
+    int result = calculatorService.add(firstNumber, secondNumber);
+    return ResponseEntity.ok(result);
   }
 
   @GetMapping("/sub")
-  public int sub(@RequestParam int firstNumber, @RequestParam int secondNumber) {
-    return firstNumber - secondNumber;
+  public ResponseEntity<Integer> sub(@RequestParam int firstNumber,
+      @RequestParam int secondNumber) {
+    int result = calculatorService.sub(firstNumber, secondNumber);
+    return ResponseEntity.ok(result);
   }
 
   @GetMapping("/mul")
-  public int mul(@RequestParam int firstNumber, @RequestParam int secondNumber) {
-    return firstNumber * secondNumber;
+  public ResponseEntity<Integer> mul(@RequestParam int firstNumber,
+      @RequestParam int secondNumber) {
+    int result = calculatorService.mul(firstNumber, secondNumber);
+    return ResponseEntity.ok(result);
   }
 
   @GetMapping("/div")
-  public int div(@RequestParam int firstNumber, @RequestParam int secondNumber) {
-    return firstNumber / secondNumber;
+  public ResponseEntity<Integer> div(@RequestParam int firstNumber,
+      @RequestParam int secondNumber) {
+    int result = calculatorService.div(firstNumber, secondNumber);
+    return ResponseEntity.ok(result);
   }
 
 }
